64a963b3d02dc95be76ffa54dac192bfef387ed1
Eniola Ajiboye <eloyjaws@gmail.com>
2021-12-07 15:36:29 +0200

    Fix circle detection - experiment with param2 in HoughCircle

--- circle_detection.py
+++ circle_detection.py
@@ -3,32 +3,64 @@ import cv2
 import cv2 as cv
 
 import matplotlib.pyplot as plt
-from config import reference, target, WIDTH
+from config import reference, target, targets, WIDTH
 import utils
+
+
+# https://docs.opencv.org/3.4/d4/d70/tutorial_hough_circle.html
+# https://docs.opencv.org/4.x/da/d53/tutorial_py_houghcircles.html
+
+
+
 
 USE_SIFT = True
 SHOW_PLOT = False
 
-# Load image
 reference_card = utils.resize(cv.imread(reference))
-image = utils.resize(cv.imread(target))
 
-corrected_image = utils.run_sift(reference_card, image, SHOW_PLOT=SHOW_PLOT) if USE_SIFT else image
-gray_image = utils.convert_to_grayscale(corrected_image)
+for target in targets[:]:
+    # Load image
+    image = utils.resize(cv.imread(target))
 
-# https://dsp.stackexchange.com/questions/22648/in-opecv-function-hough-circles-how-does-parameter-1-and-2-affect-circle-detecti
-circles = cv2.HoughCircles(gray_image, cv2.HOUGH_GRADIENT,
-                           1, 100, param1=100, param2=70, minRadius=0, maxRadius=0)
+    corrected_image = utils.run_sift(reference_card, image, SHOW_PLOT=SHOW_PLOT) if USE_SIFT else image
+    gray_image = utils.convert_to_grayscale(corrected_image)
 
-if circles is None:
-    circles = np.array([[]])
-circles = np.uint16(np.around(circles))
-padding = 8
-for (x, y, r) in circles[0]:
-    cv2.putText(corrected_image, f"{x}, {y}, {r}", (x, y),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 0, 0), 1, cv2.LINE_AA)
-    cv2.circle(corrected_image, (x, y), r-padding, (0, 255, 0), 3)
+    # https://dsp.stackexchange.com/questions/22648/in-opecv-function-hough-circles-how-does-parameter-1-and-2-affect-circle-detecti
+    
 
-cv2.imshow("Detected Circle", corrected_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # dp: This parameter is the inverse ratio of the accumulator resolution 
+    # to the image resolution (see Yuen et al. for more details). 
+    # Essentially, the larger the dp gets, the smaller the accumulator array gets.
+    dp = 1
+    # minDist: Minimum distance between the center (x, y) coordinates of detected circles. 
+    # If the minDist is too small, multiple circles in the same neighborhood as the 
+    # original may be (falsely) detected. 
+    # If the minDist is too large, then some circles may not be detected at all.
+    minDist = 100
+    # param1: Gradient value used to handle edge detection in the Yuen et al. method.
+    param1 = 100
+    # param2: Accumulator threshold value for the cv2.HOUGH_GRADIENT method. 
+    # The smaller the threshold is, the more circles will be detected (including false circles). 
+    # The larger the threshold is, the more circles will potentially be returned.
+    param2 = 75
+    # minRadius: Minimum size of the radius (in pixels).
+    minRadius = 0
+    # maxRadius: Maximum size of the radius (in pixels).
+    maxRadius = 0
+
+    circles = cv2.HoughCircles(gray_image, cv2.HOUGH_GRADIENT,
+                            dp, minDist, param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
+
+    if circles is None:
+        circles = np.array([[]])
+    circles = np.uint16(np.around(circles))
+    padding = 16
+    print(circles)
+    for (x, y, r) in circles[0]:
+        cv2.putText(corrected_image, f"{x}, {y}, {r}", (x, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 0, 0), 1, cv2.LINE_AA)
+        cv2.circle(corrected_image, (x, y), r-padding, (0, 255, 0), 3)
+
+    cv2.imshow("Detected Circle", corrected_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
