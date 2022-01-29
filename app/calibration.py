597ca19a47a2c408b11753d900a4e92f105f10c0
Eniola Ajiboye <eloyjaws@gmail.com>
2022-01-29 16:22:59 +0200

    Refactor calibration step

--- app/calibration.py
+++ app/calibration.py
@@ -8,6 +8,7 @@ from pprint import pprint
 
 import utils
 
+# Interesting links
 # http://www.ipb.uni-bonn.de/html/teaching/photo12-2021/2021-pho1-22-Zhang-calibration.pptx.pdf
 # https://docs.opencv.org/3.4.15/d9/d0c/group__calib3d.html
 # https://stackoverflow.com/questions/43563988/camera-calibration-without-chess-boards-corners
@@ -27,10 +28,10 @@ def run_calibration(args):
     (image, sorted_contours, colors) = utils.load_image_with_features(path_to_reference)
     assert(len(sorted_contours) == 30)
     assert(len(colors) == 30), "Could not extract all boxes from reference successfully"
-    objpoints = utils.flatten(sorted_contours, expand=True)
-    imgpoints = []
 
     no_of_valid_images = 0
+    imgpoints = []
+    objpoints = utils.flatten(sorted_contours, expand=True)
     for image_path in list_of_images:
         (image, sorted_contours, colors) = utils.load_image_with_features(image_path)
         assert(len(sorted_contours) == 30), f"Could not extract all boxes from {image_path} successfully"
@@ -41,30 +42,24 @@ def run_calibration(args):
     objpoints = np.array([objpoints for i in range(no_of_valid_images)])
     grayscale_image = utils.convert_to_grayscale(image)
 
-    
-
-    k = (objpoints - np.min(objpoints))/(np.max(objpoints) - np.min(objpoints))
-    l = (imgpoints - np.min(imgpoints))/(np.max(imgpoints) - np.min(imgpoints))
-
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
-        k, l, grayscale_image.shape[::-1], None, None)
     # ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
     #     objpoints, imgpoints, grayscale_image.shape[::-1], None, None)
-
-    # print(ret, "\n\n", mtx, "\n\n", dist, "\n\n", rvecs, "\n\n", tvecs)
-    print("camera parameters saved, RMS error: ", ret)
-
     # reprojection_error = utils.calculate_reprojection_error(objpoints, imgpoints, mtx, dist, rvecs, tvecs)
+    
+    # Use normalized values for calibration
+    k = (objpoints - np.min(objpoints))/(np.max(objpoints) - np.min(objpoints))
+    l = (imgpoints - np.min(imgpoints))/(np.max(imgpoints) - np.min(imgpoints))
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+        k, l, grayscale_image.shape[::-1], None, None)
     reprojection_error = utils.calculate_reprojection_error(k, l, mtx, dist, rvecs, tvecs)
-    print(f"total error: {reprojection_error}")
+
+    print("RMS error: ", ret)
+    print(f"Reprojection Error: {reprojection_error}")
 
     h,  w = image.shape[:2]
     newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 
-    reprojection_error = utils.calculate_reprojection_error(k, l, mtx, dist, rvecs, tvecs)
-    # reprojection_error = utils.calculate_reprojection_error(objpoints, imgpoints, mtx, dist, rvecs, tvecs)
-    print(f"total error after calculating optimal matrix: {reprojection_error}")
-
+    # Write out camera parameters to yaml file
     camera_parameters = {
         'ret': ret,
         'camera_matrix': np.asarray(mtx).tolist(),
@@ -74,32 +69,31 @@ def run_calibration(args):
     }
     save_camera_parameters(camera_parameters, camera_id)
 
-    if not show_results:
-        return
+    if show_results:
+        for image_path in list_of_images:
+            image = utils.resize(cv2.imread(image_path))
+            h,  w = image.shape[:2]
+            newcameramtx, roi = cv2.getOptimalNewCameraMatrix(
+                mtx, dist, (w, h), 1, (w, h))
 
-    for image_path in list_of_images:
-        image = utils.resize(cv2.imread(image_path))
-        h,  w = image.shape[:2]
-        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(
-            mtx, dist, (w, h), 1, (w, h))
+            # Option 1: undistort
+            # dst = cv2.undistort(image, mtx, dist, None, newcameramtx)
 
-        # Option 1: undistort
-        # dst = cv2.undistort(image, mtx, dist, None, newcameramtx)
+            # Option 2: remap
+            mapx, mapy = cv2.initUndistortRectifyMap(
+                mtx, dist, None, newcameramtx, (w, h), 5)
+            dst = cv2.remap(image, mapx, mapy, cv2.INTER_LINEAR)
 
-        # Option 2: remap
-        mapx, mapy = cv2.initUndistortRectifyMap(
-            mtx, dist, None, newcameramtx, (w, h), 5)
-        dst = cv2.remap(image, mapx, mapy, cv2.INTER_LINEAR)
-
-        # crop the image
-        x, y, w, h = roi
-        dst = dst[y:y+h, x:x+w]
-        utils.plot([image, dst])
+            # crop the image
+            x, y, w, h = roi
+            dst = dst[y:y+h, x:x+w]
+            utils.plot([image, dst])
 
 
 def save_camera_parameters(camera_parameters, camera_id=""):
     with open(f"data/camera_parameters_{camera_id}.yaml", 'w') as file:
         documents = yaml.dump(camera_parameters, file)
+    print(f"Camera parameters saved to: data/camera_parameters_{camera_id}.yaml")
 
 def load_camera_parameters(camera_id):
     with open(f'data/camera_parameters_{camera_id}.yaml') as file:
@@ -109,7 +103,9 @@ def load_camera_parameters(camera_id):
         dist_coeff = np.array(camera_parameters.get('dist_coeff'))
         rvecs = np.array(camera_parameters.get('rvecs'))
         tvecs = np.array(camera_parameters.get('tvecs'))
+        print(f"Camera parameters loaded from: data/camera_parameters_{camera_id}.yaml")
         return ret, camera_matrix, dist_coeff, rvecs, tvecs
+
 
 
 
